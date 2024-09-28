e89c5ff1be5173c14e992f2cb8ce53690abd8378
Sheetal Verma <148845414+Sheetalv766@users.noreply.github.com>
2024-09-28 22:38:08 +0530

    Create OrderCancellationTest.java

---
+++ testNG1/src/test/testcaseswithouttestng/OrderCancellationTest.java
@@ -0,0 +1,23 @@
+package testcaseswithouttestng;
+
+
+
+import pages.OrderCancellationPage;
+
+import org.testng.annotations.Test;
+
+public class OrderCancellationTest extends BaseTest{
+	
+	@Test (description = "Verify the order cancellation functionality", priority = 1)
+	public void verifyProductCancellation() {
+		OrderCancellationPage orderCancellationPage = new OrderCancellationPage();
+		orderCancellationPage.orderCancellation("order_987684");
+	}
+
+	@Test (enabled = false, description = "Verify the product return functionality", priority = 1, dependsOnMethods = "verifyProductCancellation")
+	public void verifyOrderReturn() {
+		OrderCancellationPage orderCancellationPage = new OrderCancellationPage();
+		orderCancellationPage.returnOrder("order_987684");
+	}
+
+}
