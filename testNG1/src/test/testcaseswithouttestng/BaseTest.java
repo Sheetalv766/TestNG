84632e0e532c56710847b6d3291dfa8f0c3a4801
Sheetal Verma <148845414+Sheetalv766@users.noreply.github.com>
2024-09-28 22:37:10 +0530

    Create BaseTest.java

---
+++ testNG1/src/test/testcaseswithouttestng/BaseTest.java
@@ -0,0 +1,36 @@
+package testcaseswithouttestng;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class BaseTest {
+
+	@BeforeSuite
+	public void launchBrowser() {
+		System.out.println("Launching browser.");
+	}
+
+	@BeforeSuite
+	public void openUrl() {
+		System.out.println("Opening url.");
+	}
+
+	@BeforeMethod 
+	public void login() {
+		System.out.println("Login into application.");
+	}
+
+	@AfterMethod
+	public void logout() {
+		System.out.println("Logout from application.");
+	}
+
+	@AfterSuite
+	public void closeBrowser() {
+		System.out.println("Closing browser");
+	}
+}
